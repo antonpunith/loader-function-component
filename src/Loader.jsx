a502d706754538e07b0e6b2d5a62a1acbf463163
Anton Punith <apunith@emsbk.com>
2019-11-13 10:55:25 +1100

    add styling to circle

--- src/Loader.jsx
+++ src/Loader.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React from "react";
+
+import "./loader.css";
 
 const Loader = () => {
-  return (<div className="spinner">
-  </div>);
-}
-export default Loader
+  const spinnerSize = 100;
+  return (
+    <div className="spinner">
+      <svg
+        focusable="false"
+        width={spinnerSize}
+        height={spinnerSize}
+        viewBox={`0 0 ${spinnerSize} ${spinnerSize}`}
+      >
+        <circle
+          cx={spinnerSize / 2}
+          cy={spinnerSize / 2}
+          r={spinnerSize / 2 - 10}
+        />
+      </svg>
+    </div>
+  );
+};
+export default Loader;
